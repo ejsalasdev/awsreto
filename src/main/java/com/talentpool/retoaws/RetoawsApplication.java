25fb0a857529cb891003db7049ea9954bc8dd3c9
Edgar J. Salas <ejsalasdev@gmail.com>
2025-07-15 20:21:34 -0500

    initial commit

---
+++ src/main/java/com/talentpool/retoaws/RetoawsApplication.java
@@ -0,0 +1,13 @@
+package com.talentpool.retoaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talentpool/retoaws/RetoawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentpool.retoaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
